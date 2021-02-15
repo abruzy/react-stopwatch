b2e053f3cd5004688f688102d9a376dee71372db
Abubakar Diallo <dialloabubakar5861@gmail.com>
2021-02-15 23:04:40 +0100

    feat: stop watch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+
+import "./App.css";
 
 function App() {
+  const [timer, setTimer] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+  const countRef = useRef(null);
+
+  const handleStart = () => {
+    setIsActive(true);
+    setIsPaused(true);
+    countRef.current = setInterval(() => {
+      setTimer((timer) => timer + 1);
+    }, 1000);
+  };
+
+  const handleReset = () => {
+    clearInterval(countRef.current);
+    setIsActive(false);
+    setIsPaused(false);
+    setTimer(0);
+  };
+
+  const handlePause = () => {
+    clearInterval(countRef.current);
+    setIsPaused(false);
+  };
+
+  const handleResume = () => {
+    setIsPaused(true);
+    countRef.current = setInterval(() => {
+      setTimer((timer) => timer + 1);
+    }, 1000);
+  };
+
+  const getSeconds = (timer) => {
+    return `0${timer % 60}`.slice(-2);
+  };
+
+  const getMinutes = (timer) => {
+    let minutes = `${Math.floor(timer / 60)}`;
+
+    return `0${minutes % 60}`.slice(-2);
+  };
+
+  const getHours = (timer) => {
+    return `0${Math.floor(timer / 3600)}`.slice(-2);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='clock'>
+        <span className='hrs' id='hrs'>
+          {getHours(timer)}
+        </span>
+        <span className='mins' id='mins'>
+          {getMinutes(timer)}
+        </span>
+        <span className='secs' id='secs'>
+          {getSeconds(timer)}
+        </span>
+      </div>
+      <div className='buttons'>
+        {!isActive && !isPaused ? (
+          <button onClick={handleStart}>Start</button>
+        ) : isPaused ? (
+          <button onClick={handlePause}>Pause</button>
+        ) : (
+          <button onClick={handleResume}>Resume</button>
+        )}
+        <button onClick={handleReset} disabled={!isActive}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
